bf4e3980b9d92ab3312c99264d8c77b4fd6addc0
stoull <changchunstep@gmail.com>
2022-04-02 18:42:12 +0800

    remove db and log files from ignore file

--- DouBanMovieSpider/middlewares.py
+++ DouBanMovieSpider/middlewares.py
@@ -77,7 +77,7 @@ class DoubanmoviespiderDownloaderMiddleware:
         # - or return a Request object
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
-        request.meta['proxy'] = 'http://xxxxx.com'
+        request.meta['proxy'] = 'http://xxx.com'
         return None
 
     def process_response(self, request, response, spider):
